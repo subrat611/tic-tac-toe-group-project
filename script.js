3a4573d618a68c23a1febf064212644f40eebf5d
subrat <subratj611@gmail.com>
2023-04-01 11:56:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ * What you will learn?
+ * 
+ * 1. using javascript you will create entire html content.
+ * 
+ * 2. using modules to separate logic (import/export).
+ * 
+ * 3. logic - random image, win/lose.
+ * 
+ * 4. html -> data attribute (custom attribute).
+ * 
+ * 5. html/css concepts.
+ * 
+ * 6. AI algorithm to play with computer.
+ * 
+ */
